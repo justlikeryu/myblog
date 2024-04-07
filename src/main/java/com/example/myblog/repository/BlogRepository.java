ca992e3701917f371b3fcc7bf656ec5c7b6fb696
justlikeryu <justlikeryuu@gmail.com>
2024-04-07 16:20:35 +0900

    Feat: 레포지토리 작성

---
+++ src/main/java/com/example/myblog/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package com.example.myblog.repository;
+
+import com.example.myblog.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
