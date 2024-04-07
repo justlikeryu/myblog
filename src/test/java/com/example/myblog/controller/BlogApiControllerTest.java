6875274add134ee3ae8570685978a34363e964c9
justlikeryu <justlikeryuu@gmail.com>
2024-04-08 00:01:15 +0900

    Test: 블로그 포스팅 API 테스트 코드

--- src/test/java/com/example/myblog/controller/BlogApiControllerTest.java
+++ src/test/java/com/example/myblog/controller/BlogApiControllerTest.java
@@ -1,14 +1,26 @@
 package com.example.myblog.controller;
 
+import com.example.myblog.domain.Article;
+import com.example.myblog.dto.AddArticleRequest;
 import com.example.myblog.repository.BlogRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest//테스트용 애플리케이션 컨텍스트
 @AutoConfigureMockMvc
@@ -30,5 +42,33 @@ class BlogApiControllerTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
                 .build();
         blogRepository.deleteAll();
+    }
+
+    @DisplayName("addArticle: 블로그 포스팅 성공")
+    @Test
+    public void addArticle() throws Exception{
+        //given
+        final String url ="/api/articles";
+        final String title = "title";
+        final String content = "content";
+        final AddArticleRequest addArticleRequest = new AddArticleRequest(title, content);
+
+        //json으로 직렬화
+        final String requestBody =objectMapper.writeValueAsString(addArticleRequest);
+
+        //when
+        //mockMvc를 이용해 http method, url, request type등을 설정한 뒤 해당 내용을 바탕으로 요청을 전송한다
+        ResultActions resultActions = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)//JSON타입의 요청을 보냄
+                .content(requestBody));
+
+        //then
+        resultActions.andExpect(status().isCreated());
+
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles.size()).isEqualTo(1);//블로그 개수가 1인지 검증
+        assertThat(articles.get(0).getTitle()).isEqualTo(title);
+        assertThat(articles.get(0).getContent()).isEqualTo(content);
     }
 }
