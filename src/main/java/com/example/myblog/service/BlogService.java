df95680a5b8425575cf3d1e064b5068ddf6ed938
justlikeryu <justlikeryuu@gmail.com>
2024-04-08 01:14:12 +0900

    Feat: 블로그 글 삭제 API

--- src/main/java/com/example/myblog/service/BlogService.java
+++ src/main/java/com/example/myblog/service/BlogService.java
@@ -28,4 +28,9 @@ public class BlogService {
         return blogRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found:" + id));
     }
+
+    //블로그 글 삭제
+    public void delete(long id){
+        blogRepository.deleteById(id);
+    }
 }
