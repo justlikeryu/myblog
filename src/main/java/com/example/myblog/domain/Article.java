fd7af218f94d92460db3ecf804480025e9f5e485
justlikeryu <justlikeryuu@gmail.com>
2024-04-07 16:17:30 +0900

    Feat: 엔터티 작성

---
+++ src/main/java/com/example/myblog/domain/Article.java
@@ -0,0 +1,29 @@
+package com.example.myblog.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder//빌더 패턴으로 객체 생성, 빌더 패턴을 사용하면 가독성이 높아진다
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
