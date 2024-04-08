11dae9eff72ffda663b82f4049b74903070bdd6d
justlikeryu <justlikeryuu@gmail.com>
2024-04-08 11:38:27 +0900

    Feat: 블로그 글 삭제

---
+++ src/main/resources/static/js/article.js
@@ -0,0 +1,14 @@
+const delBtn = document.getElementById('delete-btn');
+
+if (delBtn) {
+    delBtn.addEventListener('click', event => {
+        let id = document.getElementById('article-id').value;
+        fetch(`/api/articles/${id}`, {
+            method: 'DELETE'
+        })
+            .then(() => {
+                alert('삭제가 완료되었습니다.');
+                location.replace('/articles');
+            });
+    });
+}
