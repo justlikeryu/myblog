d140558b8c028694d3addbcb0751b3214acca8c2
justlikeryu <justlikeryuu@gmail.com>
2024-04-08 11:24:05 +0900

    Feat: 블로그 글 뷰

--- src/main/java/com/example/myblog/MyblogApplication.java
+++ src/main/java/com/example/myblog/MyblogApplication.java
@@ -2,8 +2,10 @@ package com.example.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing//엔터티 생성일시와 수정일시를 자동으로 업데이트 하기 위한 어노테이션
 public class MyblogApplication {
 
     public static void main(String[] args) {
