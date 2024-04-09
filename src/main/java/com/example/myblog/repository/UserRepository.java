a22a1b6683c2761a7062bdcf8864bd07b5c0addb
justlikeryu <justlikeryuu@gmail.com>
2024-04-09 16:26:18 +0900

    Feat: 유저 레포지토리 추가

--- src/main/java/com/example/myblog/repository/UserRepository.java
+++ src/main/java/com/example/myblog/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.example.myblog.repository;public interface UserRepository {
+package com.example.myblog.repository;
+
+import com.example.myblog.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);//이메일로 사용자 식별
 }
