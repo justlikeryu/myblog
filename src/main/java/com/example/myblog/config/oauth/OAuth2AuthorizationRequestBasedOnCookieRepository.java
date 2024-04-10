12790c60ce5b6b967d74f6fa72c312cc98653ebf
justlikeryu <justlikeryuu@gmail.com>
2024-04-10 21:42:40 +0900

    Feat: 인증 성공/실패 시 실행할 핸들러 구현

---
+++ src/main/java/com/example/myblog/config/oauth/OAuth2AuthorizationRequestBasedOnCookieRepository.java
@@ -0,0 +1,43 @@
+package com.example.myblog.config.oauth;
+
+import com.example.myblog.util.CookieUtil;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.web.util.WebUtils;
+
+// /api로 시작하는 url은 인증에 실패하면 401 상태코드, Unauthorized를 반환한다
+//OAuth2에 필요한 정보를 세션이 아닌 쿠키에 저장해서 사용할 수 있게 인증 요청과 관련된 상태를 저장할 저장소를 구현해야 한다.
+//권한 인증 흐름에서 클라이언트의 요청을 유지하는데 사용되는 클래스가 해당 클래스이다.
+public class OAuth2AuthorizationRequestBasedOnCookieRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
+    public final static String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+    public final static int COOKIE_EXPIRE_SECONDS = 18000;
+
+    @Override
+    public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
+        Cookie cookie = WebUtils.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+
+        return CookieUtil.deserialize(cookie, OAuth2AuthorizationRequest.class);
+    }
+
+    @Override
+    public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
+        if (authorizationRequest == null) {
+            removeAuthorizationRequest(request, response);
+            return;
+        }
+
+        CookieUtil.addCookie(response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME, CookieUtil.serialize(authorizationRequest), COOKIE_EXPIRE_SECONDS);
+    }
+
+    @Override
+    public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
+        return this.loadAuthorizationRequest(request);
+    }
+
+    public void removeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response) {
+        CookieUtil.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+    }
+}
