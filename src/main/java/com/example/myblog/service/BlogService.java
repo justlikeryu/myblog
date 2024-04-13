d12c0925fe65b8a004ed7e3e580c8dc3ddcb398f
justlikeryu <justlikeryuu@gmail.com>
2024-04-13 18:28:22 +0900

    Fix: 블로그 글 삭제 되도록 수정

--- src/main/java/com/example/myblog/service/BlogService.java
+++ src/main/java/com/example/myblog/service/BlogService.java
@@ -37,6 +37,9 @@ public class BlogService {
     public void delete(long id) {
         Article article = blogRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found:" + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
     //블로그 글 수정
